bd5fcb7005b0921f7e2f2eea2255b0a5909abbd4
Reggio <contact@graphandco.com>
2020-05-03 15:10:40 +0200

    Add CORS and change port to 8080

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+app.use(cors());
 
-app.use(express.json())
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
 
-const subscribersRouter = require('./routes/subscribers')
-app.use('/subscribers', subscribersRouter)
+app.use(express.json());
 
-app.listen(3000, () => console.log('Server Started'))
+const subscribersRouter = require('./routes/subscribers');
+app.use('/subscribers', subscribersRouter);
+
+app.listen(8080, () => console.log('Server Started'));
